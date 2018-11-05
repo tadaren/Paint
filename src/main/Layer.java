06e026e0128944caacd6f9388f4ce91f72c07d0b
tadaren <ptymaato@yahoo.co.jp>
2018-11-05 11:34:31 +0900

    [fix] 背景色が透明の時に消しゴムと全消去が働かないバグを修正

--- src/main/Layer.java
+++ src/main/Layer.java
@@ -25,6 +25,10 @@ public class Layer {
 		Graphics2D g = image.createGraphics();
 		g.setColor(defaultColor);
 		g.fillRect(0, 0, image.getWidth(), image.getHeight());
+		if(defaultColor.getRGB() == 0){
+			g.setBackground(new Color(0, 0, 0, 0));
+			g.clearRect(0, 0, image.getWidth(), image.getHeight());
+		}
 	}
 
 	// Layerが保持しているBufferedImageを返す
--- src/pen/Eraser.java
+++ src/pen/Eraser.java
@@ -14,6 +14,10 @@ public class Eraser extends DefaultPen {
 		g.setColor(layer.getDefaultColor());
 		g.setStroke(PenManager.getInstance().getStroke());
 		g.drawLine(e.getX(), e.getY(), getPointBuffer().x, getPointBuffer().y);
+		if(layer.getDefaultColor().getRGB() == 0){
+			g.setBackground(new Color(0, 0, 0, 0));
+			g.clearRect(e.getX()-2, e.getY()-2, Math.abs(getPointBuffer().x-e.getX()+5), Math.abs(getPointBuffer().y-e.getY()+5));
+		}
 		setPointBuffer(e.getPoint());
 	}
 
